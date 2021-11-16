472e2f310a588470e273ee2cb45147681c1a0b4a
ozer0532 <ozer0532@gmail.com>
2021-11-16 19:24:57 +0700

    Fix grammar and add train dict

--- train.sh
+++ train.sh
@@ -23,16 +23,16 @@ mkdir -p temp/triphones/
 
 # Step 1
 # $1 = sample
-julia ./scripts/mkdfa.jl $1
+julia scripts/mkdfa.jl $1
 
 # Step 2
 # $2 = prompts.txt
-julia ../bin/prompts2wlist.jl $2
+julia scripts/prompts2wlist.jl $2
 HDMan -A -D -T 1 -m -w temp/pronunciation/wlist -n temp/pronunciation/monophones1 -i -l temp/pronunciation/dlog temp/pronunciation/dict model/VoxForgeDict.txt
 sed '/sp/d' ./temp/pronunciation/monophones1 > ./temp/pronunciation/monophones0
 
 # Step 4
-julia ../bin/prompts2mlf.jl $2
+julia scripts/prompts2mlf.jl $2
 HLEd -A -D -T 1 -l '*' -d temp/pronunciation/dict -i temp/transcription/phones0.mlf scripts/mkphones0.led temp/transcription/words.mlf
 HLEd -A -D -T 1 -l '*' -d temp/pronunciation/dict -i temp/transcription/phones1.mlf scripts/mkphones1.led temp/transcription/words.mlf
 
