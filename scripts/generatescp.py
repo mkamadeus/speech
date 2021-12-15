8d7d6dd08da7ecf21fb619d06444fbde4f79dfbb
ozer0532 <ozer0532@gmail.com>
2021-12-15 18:02:55 +0700

    Modify scp gen

--- scripts/generatescp.py
+++ scripts/generatescp.py
@@ -14,8 +14,8 @@ PATH_TO_TRAIN = 'temp/hmm/train.scp'
 with open(PATH_TO_PROMPTS, 'r') as f:
 	lines = list(map(lambda x: x.split(' ')[0], f.readlines()))
 	lines = [l.replace('*', 'train') for l in lines]
-	codetrain_lines = [f"{l}.wav temp/encoding/{l.split('/')[-1]}.mfc" for l in lines]
-	train_lines = [f"temp/encoding/{l.split('/')[-1]}.mfc" for l in lines]
+	codetrain_lines = [f"{l}.wav temp/encoding/{'-'.join(l.lower().split('/')[1:])}.mfc" for l in lines]
+	train_lines = [f"temp/encoding/{'-'.join(l.lower().split('/')[1:])}.mfc" for l in lines]
 
 with open(PATH_TO_CODETRAIN, 'w') as f:
 	f.write('\n'.join(codetrain_lines))
